8bc3dcc2b96b1f50cf5f933eb74799b0bdb9267a
Dhia Djobbi <badboy.dhia@gmail.com>
2020-11-06 00:43:45 +0100

    Revert to original

--- src/main/java/com/dhia/factures/FacturesApplication.java
+++ src/main/java/com/dhia/factures/FacturesApplication.java
@@ -1,30 +1,19 @@
 package com.dhia.factures;
 
-import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.dhia.factures.entities.Facture;
-import com.dhia.factures.services.FactureService;
+
 
 @SpringBootApplication
-public class FacturesApplication implements CommandLineRunner {
 
-	@Autowired
-	FactureService factureService;
+public class FacturesApplication{
+
 	public static void main(String[] args) {
 		SpringApplication.run(FacturesApplication.class, args);
 	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-	factureService.saveFacture(new Facture("facture4", 4600.0, new Date()));
-	factureService.saveFacture(new Facture("facture5", 500.0, new Date()));
-	factureService.saveFacture(new Facture("facture9", 58850.0, new Date()));
-	factureService.saveFacture(new Facture("facture8", 58050.0, new Date()));
-	}
+
 
 }
